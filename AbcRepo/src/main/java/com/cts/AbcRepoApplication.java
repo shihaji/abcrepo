87a9e698ae83a4197dffb5ecee7fe3048ccd1c4c
shihaji <shihajig@gmail.com>
2021-03-19 19:48:14 +0530

    commit

---
+++ AbcRepo/src/main/java/com/cts/AbcRepoApplication.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcRepoApplication.class, args);
+	}
+
+}
